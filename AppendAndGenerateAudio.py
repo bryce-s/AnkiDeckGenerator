0fc3ec1fcee134e8f570086791ca375f742a9ba2
bryce-s <bryces@umich.edu>
2019-10-27 19:12:24 -0400

    want to add mirroring feature and name deck feature

--- AppendAndGenerateAudio.py
+++ AppendAndGenerateAudio.py
@@ -3,7 +3,22 @@ import os
 from gtts import gTTS
 import csv
 import json
+import uuid
+import genanki
+import shutil
+import random
 
+
+class CardInfo:
+    frontText = ""
+    backText = ""
+    frontAudio = ""
+    backAudio = ""
+    def __init__(self, frontText: str, backText: str, frontAudio: str, backAudio: str):
+        self.frontText = frontText
+        self.backText = backText
+        self.frontAudio = frontAudio
+        self.backAudio = backAudio
 
 class AudioGenerator:
 
@@ -14,6 +29,9 @@ class AudioGenerator:
         self.backLanguage: str = ""
         self.assignFrontAndBackLanguages()
         self.createAudioOutputDir()
+        self.cardAudioStore: list = list()
+        self.model = None
+        self.mediaFilePaths: list = list()
 
     def assignFrontAndBackLanguages(self):
         with open("settings.json") as settingsFile:
@@ -25,7 +43,7 @@ class AudioGenerator:
     def createAudioOutputDir(self):
         """delete and re-create output dir.. otherwise just create it"""
         if os.path.isdir(self.outputDir):
-            os.rmdir(self.outputDir)
+            shutil.rmtree(self.outputDir)
         os.mkdir(self.outputDir)
 
 
@@ -34,16 +52,67 @@ class AudioGenerator:
             csvReader = csv.reader(f, delimiter=',')
             rowNum = 0
             for row in csvReader:
+                rowNum += 1
                 frontSide: str = row[0]
                 backSide: str = row[1]
                 print(f'row {str(rowNum)}: front: {frontSide}, back: {backSide}')
-                frontAudio = gTTS(text=frontSide, lang=self.frontLanguage, slow=False)
-                backAudio = gTTS(text=backSide, lang=self.backLanguage, slow=False)
-                
+                try:
+                    frontAudio = gTTS(text=frontSide, lang=self.frontLanguage, slow=False)
+                    backAudio = gTTS(text=backSide, lang=self.backLanguage, slow=False)
+                except AssertionError:
+                    print(AssertionError)
+                    print("Probably input is malformatted. Check line {}".format(rowNum))
+                    exit(1)
+
                 # the filenames have to be unique--what can we do here?
+                frontUUID: str = uuid.uuid1()
+                frontFilename: str = "{}.mp3".format(frontUUID)
 
+                backUUID: str = uuid.uuid1()
+                backFilename: str = "{}.mp3".format(backUUID)
+                frontAudioPath = "{}{}".format(self.outputDir, frontFilename)
+                frontAudio.save(frontAudioPath)
+                backAudioPath = "{}{}".format(self.outputDir, backFilename)
+                backAudio.save(backAudioPath)
+                self.mediaFilePaths.append(frontAudioPath)
+                self.mediaFilePaths.append(backAudioPath)
+                self.cardAudioStore.append(CardInfo(frontSide, backSide, frontFilename, backFilename))
     
+    def __buildGenankiModel(self):
+        cssStr=".card { text-align: center; font-size: 24px; }"
+        self.model = genanki.Model(
+                    1607392219,
+                    'Simple Model',
+                    css=cssStr,
+                    fields=[
+                      {'name': 'Question'},
+                      {'name': 'FrontAudio'},
+                      {'name': 'Answer'},
+                      {'name': 'BackAudio'}
+                    ],
+                    templates=[
+                      {
+                          'name': 'Card 1',
+                          'qfmt': '<div class="basicCenter">{{Question}}<br>{{FrontAudio}}</div>',
+                          'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}<br>{{BackAudio}}',
+                        },
+                    ])
 
+    def buildGenankiDeck(self):
+        self.__buildGenankiModel()
+        deckID: int = random.randrange(1 << 30, 1 << 31)
+        newDeck = genanki.Deck(deckID, 'Deck-'+str(deckID))
+        for cardInfo in self.cardAudioStore:
+            newDeck.add_note(genanki.Note(model=self.model, fields=[
+                cardInfo.frontText, 
+                "[sound:{}]".format(cardInfo.frontAudio), 
+                cardInfo.backText, 
+                "[sound:{}]".format(cardInfo.backAudio)
+                ]
+                ))
+        pack = genanki.Package(newDeck)
+        pack.media_files = self.mediaFilePaths
+        pack.write_to_file('output.apkg')
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -52,4 +121,5 @@ if __name__ == "__main__":
     file: str = sys.argv[1]
     gen: AudioGenerator = AudioGenerator(file)
     gen.generateAudio()
+    gen.buildGenankiDeck()
 
